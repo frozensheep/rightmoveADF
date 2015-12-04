84d8a745b5ff30f19ab0d5f0b2d24c86e33c088e
Jacob Wyke <jacob@frozensheep.com>
2015-12-04 12:35:42 +0000

    Adding example for getBranchPerformance request.

---
+++ examples/getBranchPerformance.php
@@ -0,0 +1,32 @@
+<?php
+/**
+*	Remove Property Example.
+*
+*	@package	Frozensheep\RightmoveADF
+*	@author		Jacob Wyke <jacob@frozensheep.com>
+*	@license	MIT
+*
+*/
+
+require_once(__DIR__.'/../vendor/autoload.php');
+
+use Frozensheep\RightmoveADF\RightmoveADF;
+
+//include the config file
+require_once('config.php');
+
+//create the RightmoveADF object
+$objRightmoveADF = new RightmoveADF(CERT_FILE, CERT_PASS, RightmoveADF::TEST);
+
+//create a request
+$objRequest = $objRightmoveADF->createRequest(RightmoveADF::GetBranchPerformance);
+
+//set the details for the request
+$objRequest->network->network_id = NETWORK_ID;
+$objRequest->branch->branch_id = BRANCH_ID;
+$objRequest->export_date = 'last friday';
+
+//send the request
+$objResponse = $objRightmoveADF->send($objRequest);
+
+print_r($objResponse);
