c1195b8157447544b2ad07b1f1ea0df0fbe692c8
Jacob Wyke <jacob@frozensheep.com>
2015-12-04 12:27:04 +0000

    Updating to use the new property ref group.

--- src/Request/RemoveFeaturedProperty.php
+++ src/Request/RemoveFeaturedProperty.php
@@ -13,7 +13,7 @@ namespace Frozensheep\RightmoveADF\Request;
 use Frozensheep\RightmoveADF\Request\RequestBase;
 use Frozensheep\RightmoveADF\Groups\Network;
 use Frozensheep\RightmoveADF\Groups\Branch;
-use Frozensheep\RightmoveADF\Groups\PropertyFeature;
+use Frozensheep\RightmoveADF\Groups\PropertyRef;
 
 /**
 *	Remove Featured Property Class
@@ -41,6 +41,6 @@ class RemoveFeaturedProperty extends RequestBase {
 	protected $arrSynthesize = array(
 		'network' => array('type' => 'object', 'class' => 'Frozensheep\RightmoveADF\Groups\Network', 'required' => true),
 		'branch' => array('type' => 'object', 'class' => 'Frozensheep\RightmoveADF\Groups\Branch', 'required' => true),
-		'property' => array('type' => 'object', 'class' => 'Frozensheep\RightmoveADF\Groups\PropertyFeature', 'required' => true)
+		'property' => array('type' => 'object', 'class' => 'Frozensheep\RightmoveADF\Groups\PropertyRef', 'required' => true)
 	);
 }
