59aef52f224e39fcc2d13e5ed64a87612cfcee8e
Jacob Wyke <jacob@frozensheep.com>
2015-12-04 11:15:46 +0000

    Finishing off the rightmove adf class and splitting it into a curl class to make the actual call.

--- src/RightmoveADF.php
+++ src/RightmoveADF.php
@@ -10,6 +10,23 @@
 
 namespace Frozensheep\RightmoveADF;
 
+use Frozensheep\Synthesize\Synthesizer;
+use Frozensheep\RightmoveADF\Exception\UnknownRequestTypeException;
+use Frozensheep\RightmoveADF\Curl;
+use Frozensheep\RightmoveADF\Request\SendProperty;
+use Frozensheep\RightmoveADF\Request\RemoveProperty;
+use Frozensheep\RightmoveADF\Request\GetBranchPropertyList;
+use Frozensheep\RightmoveADF\Request\AddPremiumListing;
+use Frozensheep\RightmoveADF\Request\AddFeraturedProperty;
+use Frozensheep\RightmoveADF\Request\RemoveFeaturedProperty;
+use Frozensheep\RightmoveADF\Request\GetPropertyPerformance;
+use Frozensheep\RightmoveADF\Request\GetBranchPerformance;
+use Frozensheep\RightmoveADF\Request\GetBrandEmails;
+use Frozensheep\RightmoveADF\Request\GetBranchEmails;
+use Frozensheep\RightmoveADF\Request\GetBrandPhoneLeads;
+use Frozensheep\RightmoveADF\Request\GetBranchPhoneLeads;
+use Frozensheep\RightmoveADF\Request\GetPropertyEmail;
+
 /**
 *	RightmoveADF Class
 *
@@ -19,6 +36,8 @@ namespace Frozensheep\RightmoveADF;
 *
 */
 class RightmoveADF {
+
+	use Synthesizer;
 
 	const TEST = 0;
 	const LIVE = 1;
@@ -37,15 +56,94 @@ class RightmoveADF {
 	const GetBranchPhoneLeads = 12;
 	const GetPropertyEmail = 13;
 
-	public function __constructor($strPEMFile, $strPEMKeyFile, $numPlatform = RightmoveADF::LIVE){
+	/**
+	*	@var array $arrSynthesize The synthesize array.
+	*/
+	protected $arrSynthesize = array(
+		'cert_file' => array('type' => 'string'),
+		'cert_pass' => array('type' => 'string'),
+		'environment' => array('type' => 'int', 'min' => 0, 'max' => 1, 'default' => RightmoveADF::TEST)
+	);
 
+	/**
+	*	Class Constructor
+	*
+	*	Class constructure which accepts the certificate file, password and enviroment.
+	*	@param string $strCertFile The certificate file.
+	*	@param string $strCertPass The certificate password.
+	*	@param int $numEnvironment The environment to run in.
+	*	@return self
+	*/
+	public function __construct($strCertFile, $strCertPass, $numEnvironment = RightmoveADF::TEST){
+		$this->cert_file = $strCertFile;
+		$this->cert_pass = $strCertPass;
+		$this->environment = $numEnvironment;
 	}
 
-	public function createRequest(){
-
+	/**
+	*	Create Request Method
+	*
+	*	Returns the request object for the given request type.
+	*	@param int $numRequestType The request type.
+	*	@return object
+	*/
+	public static function createRequest($numRequestType){
+		switch($numRequestType){
+			case RightmoveADF::SendProperty:
+				return new SendProperty();
+				break;
+			case RightmoveADF::RemoveProperty:
+				return new RemoveProperty();
+				break;
+			case RightmoveADF::GetBranchPropertyList:
+				return new GetBranchPropertyList();
+				break;
+			case RightmoveADF::AddPremiumListing:
+				return new AddPremiumListing();
+				break;
+			case RightmoveADF::AddFeraturedProperty:
+				return new AddFeraturedProperty();
+				break;
+			case RightmoveADF::RemoveFeaturedProperty:
+				return new RemoveFeaturedProperty();
+				break;
+			case RightmoveADF::GetPropertyPerformance:
+				return new GetPropertyPerformance();
+				break;
+			case RightmoveADF::GetBranchPerformance:
+				return new GetBranchPerformance();
+				break;
+			case RightmoveADF::GetBrandEmails:
+				return new GetBrandEmails();
+				break;
+			case RightmoveADF::GetBranchEmails:
+				return new GetBranchEmails();
+				break;
+			case RightmoveADF::GetBrandPhoneLeads:
+				return new GetBrandPhoneLeads();
+				break;
+			case RightmoveADF::GetBranchPhoneLeads:
+				return new GetBranchPhoneLeads();
+				break;
+			case RightmoveADF::GetPropertyEmail:
+				return new GetPropertyEmail();
+				break;
+			default:
+				throw new UnknownRequestTypeException();
+				break;
+		}
 	}
 
-	public function send(){
+	/**
+	*	Create Request Method
+	*
+	*	Returns the request object for the given request type.
+	*	@param int $numRequestType The request type.
+	*	@return object
+	*/
+	public function send($objRequest, $strURLOverride = ''){
+		$strURL = ($strURLOverride) ? $strURLOverride : $objRequest->getURL($this->environment);
 
+		return Curl::send(json_encode($objRequest), $strURL, $this->cert_file, $this->cert_pass);
 	}
 }
--- src/curl.php
+++ src/curl.php
@@ -1,19 +1,49 @@
-$pemfile = "cert.pem";
-$keyfile = "private_key.key";
-$url = "someTestUrl";
-$requestXml = "requestData";
+<?php
+/**
+*	This file contains the Curl class.
+*
+*	@package	Frozensheep\RightmoveADF
+*	@author		Jacob Wyke <jacob@frozensheep.com>
+*	@license	MIT
+*
+*/
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
-curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-curl_setopt($ch, CURLOPT_SSLCERT, $pemfile);
-curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
-curl_setopt($ch, CURLOPT_SSLKEY, $keyfile);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $requestXml);
-$ret = curl_exec($ch);
+namespace Frozensheep\RightmoveADF;
+
+/**
+*	Curl Class
+*
+*	Class for sending the request over curl.
+*
+*	@package	Frozensheep\RightmoveADF
+*
+*/
+class Curl {
+
+	/**
+	*	Create Request Method
+	*
+	*	Returns the request object for the given request type.
+	*	@param int $numRequestType The request type.
+	*	@return object
+	*/
+	public static function send($strJSON, $strURL, $strCertFile, $strCertPass){
+		$resCurl = curl_init();
+
+		curl_setopt($resCurl, CURLOPT_URL, $strURL);
+		curl_setopt($resCurl, CURLOPT_CERTINFO, 1);
+		curl_setopt($resCurl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($resCurl, CURLOPT_SSL_VERIFYPEER, 1);
+		curl_setopt($resCurl, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($resCurl, CURLOPT_FAILONERROR, 1);
+		curl_setopt($resCurl, CURLOPT_SSLCERT, $strCertFile);
+		curl_setopt($resCurl, CURLOPT_SSLCERTTYPE, 'PEM');
+		curl_setopt($resCurl, CURLOPT_SSLCERTPASSWD, $strCertPass);
+		curl_setopt($resCurl, CURLOPT_POST, 1);
+		curl_setopt($resCurl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($resCurl, CURLOPT_POSTFIELDS, $strJSON);
+		$strResponse = curl_exec($resCurl);
+
+		return json_decode($strResponse);
+	}
+}
