f7b2cce836b094b08975e1ffc7823528bb07430f
Jacob Wyke <jacob@frozensheep.com>
2015-12-04 12:19:34 +0000

    Added addFeaturedProperty example.

---
+++ examples/addFeaturedProperty.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*	Add Featured Property Example.
+*
+*	@package	Frozensheep\RightmoveADF
+*	@author		Jacob Wyke <jacob@frozensheep.com>
+*	@license	MIT
+*
+*/
+
+require_once(__DIR__.'/../vendor/autoload.php');
+
+use Frozensheep\RightmoveADF\RightmoveADF;
+
+//include the config file
+require_once('config.php');
+
+//create the RightmoveADF object
+$objRightmoveADF = new RightmoveADF(CERT_FILE, CERT_PASS, RightmoveADF::TEST);
+
+//create a request
+$objRequest = $objRightmoveADF->createRequest(RightmoveADF::AddFeaturedProperty);
+
+//set the details for the request
+$objRequest->network->network_id = NETWORK_ID;
+$objRequest->branch->branch_id = BRANCH_ID;
+$objRequest->branch->channel = Frozensheep\RightmoveADF\Values\Channels::Lettings;
+$objRequest->property->agent_ref = PROPERTY_ID;
+$objRequest->property->featured_property->featured_property_type = Frozensheep\RightmoveADF\Values\FeaturedPropertyTypes::FeaturedPropertyOfTheWeek;
+
+//send the request
+$objResponse = $objRightmoveADF->send($objRequest);
+
+print_r($objResponse);
